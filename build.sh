773c15eb432f041f40b902be32ca0ad5fa7f9a33
sudheerttech-eng <sudheert.tech@gmail.com>
2026-04-15 14:18:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,23 +11,3 @@ docker login
 
 echo "🏗️ Building Docker image..."
 docker build -t $DOCKER_USERNAME/$IMAGE_NAME:$TAG .
-
-# -------------------------------
-# Push to DEV (Public)
-# -------------------------------
-echo "🏷️ Tagging for DEV repo..."
-docker tag $DOCKER_USERNAME/$IMAGE_NAME:$TAG $DOCKER_USERNAME/dev:$TAG
-
-echo "🚀 Pushing to DEV (public repo)..."
-docker push $DOCKER_USERNAME/dev:$TAG
-
-# -------------------------------
-# Push to PROD (Private)
-# -------------------------------
-echo "🏷️ Tagging for PROD repo..."
-docker tag $DOCKER_USERNAME/$IMAGE_NAME:$TAG $DOCKER_USERNAME/prod:$TAG
-
-echo "🚀 Pushing to PROD (private repo)..."
-docker push $DOCKER_USERNAME/prod:$TAG
-
-echo "✅ Image pushed to BOTH dev & prod repos!"
